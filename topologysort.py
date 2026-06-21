ef36414da5ddf68dd379845ad12a1ae1a16aa26b
Younes <hachimiyounes1337@gmail.com>
2026-06-21 08:29:10 +0100

    Update topologysort.py

--- topologysort.py
+++ topologysort.py
@@ -1,30 +1,41 @@
-
 def topological_sort(pkgs: dict[str, list[str]]) -> list[str]:
     result: list[str] = []
     visited: set[str] = set()
+    visiting = set()
 
     def dfs(pkg: str):
         if pkg in visited:
-            return
+            return True
 
-        visited.add(pkg)
+        if pkg in visiting:
+            return False
+
+        if pkg not in pkgs.keys():
+            return True
+
+        visiting.add(pkg)
 
         for dep in sorted(pkgs.get(pkg, [])):
-            dfs(dep)
+            if not dfs(dep):
+                return False
 
+        visited.add(pkg)
         result.append(pkg)
-
+        visiting.remove(pkg)
+        return True
     for pkg in pkgs.keys():
-        dfs(pkg)
+        if not dfs(pkg):
+            return []
 
     return result
 
 
-depend_pkg: dict[str, list[str]] = {}
-depend_pkg["a"] = ["c", "d", "k", "b"]
-depend_pkg["e"] = ["c", "a"]
-depend_pkg["c"] = ["d"]
-depend_pkg["g"] = ["n", "k"]
+depend_pkg = {
+    "a": ["c", "d", "k", "b"],
+    "e": ["c", "a"],
+    "c": ["d"],
+    "g": ["n", "k"],
+}
 
 
 print(topological_sort(depend_pkg))
