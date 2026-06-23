f2b55c15684a41ac586a306e0e416ba019f5a168
Younes <hachimiyounes1337@gmail.com>
2026-06-23 23:32:39 +0100

    Create rotations.py

---
+++ rotations.py
@@ -0,0 +1,18 @@
+def is_rotations(arr, arr2):
+    if len(arr) != len(arr2) or sorted(arr) != sorted(arr2):
+        return False
+
+
+    tmp = arr2[:]
+    for i in  range(len(arr)):
+        d = tmp.pop()
+        tmp.insert(0,d)
+        if tmp == arr:
+            return True 
+
+    
+    return False
+
+a = [1,2,3,4,5]
+b = [4,3,5,1,2]
+print(is_rotations(a,b))
