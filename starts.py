bcac9a5735359e7c91daeb3aeaba074193994e9d
Youness Hachimi <yhachimi@c1r2p12.1337.ma>
2026-06-24 15:36:27 +0100

    build:  adding the exam stars excercice

---
+++ starts.py
@@ -0,0 +1,19 @@
+def stars(cords: list[tuple[int, int]], size: int) -> list[str]:
+    data = {i: list("." * size) for i in range(size)}
+
+    try:
+
+        for x,y in cords:
+            if x >= 0 < size and y >= 0 < size:
+                data[x][y] = "*"
+
+        return  ["".join(data[i]) for i in range(size)]
+    except:
+       return  ["".join(data[i]) for i in range(size)]
+
+
+
+
+
+if __name__ == "__main__":
+    print(stars([(0,2), (0,0), (-1,6)], 3))
